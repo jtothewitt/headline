4214a285dd86997bb0545b343057def3b171dcc5
John Witt <jwitt10@gmail.com>
2017-03-15 21:11:37 -0700

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,8 +1,8 @@
-﻿    var words = ['oh, hello', 'salut', 'hola', 'hoi', 'ciáo', 'hallo', 'oi'], i=0;
+﻿    var words = ["hey, go get some rest, you need to make a difference tomorrow", "why are you up?", "top of the mornin' to ya", "have a great day", "keep goin', you got this", "enjoy you're evening"], i=0;
   
  setInterval(function() {         // \/ \/ callback function
   $('#greetingHop').fadeOut(900, function() {
                       // if i = last index ? i = 0 else i++
     $(this).text(words[ (i === words.length - 1) ? i = 0 : i += 1] ).fadeIn(500);
   });
-}, 2000);
+}, 14400000);
